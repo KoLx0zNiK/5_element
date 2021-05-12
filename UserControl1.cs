881c53f8a8f0a80e2b7c49f07684951d436965f4
Neo <Neo@DESKTOP-H6916IN>
2021-05-12 17:18:30 +0300

    2.0

--- UserControl1.cs
+++ UserControl1.cs
@@ -1,17 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace _5_element
 {
-    public partial class MyControlElement: UserControl
+    public partial class MyControlElement : UserControl
     {
+        Bitmap bmp;
+        Graphics gr;
+        int cellX, cellY;
+        bool choise = false;
+        Pen penCell;
         public MyControlElement()
         {
             InitializeComponent();
@@ -23,7 +22,7 @@ namespace _5_element
             pictureBox1.Top = 0;
             pictureBox1.Width = this.Width - 50;
             pictureBox1.Height = this.Height - 50;
-            
+
             //zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
 
             trackBar1.Left = 0;
@@ -55,6 +54,51 @@ namespace _5_element
             trackBar2.Left = pictureBox1.Width + 5;
             trackBar2.Top = 0;
             trackBar2.Height = pictureBox1.Height;
+        }
+
+        private void ReDrawPicture()
+        {
+            gr.Clear(pictureBox1.BackColor);
+            if (choise)
+            {
+                for (int i = 0; i <= cellX; i++)
+                {
+                    gr.DrawLine(penCell, (int)(bmp.Width * i / cellX), 0, (int)(bmp.Width * i / cellX), bmp.Height);
+                }
+                for (int i = 0; i <= cellY; i++)
+                {
+                    gr.DrawLine(penCell, 0, (int)(bmp.Height * i / cellY), bmp.Width, (int)(bmp.Height * i / cellY));
+                }
+            }
+
+
+
+
+
+            pictureBox1.Image = bmp;
+        }
+        public void InitGraph()
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            gr = Graphics.FromImage(bmp);
+        }
+        public void SetBackColor(Color cl)
+        {
+            pictureBox1.BackColor = cl;
+        }
+        public void Addcell(int x, int y, Color cl, int W)
+        {
+            choise = true;
+            penCell = new Pen(cl, W);
+            cellX = x;
+            cellY = y;
+            ReDrawPicture();
+        }
+        public void DeleteCell()
+        {
+            choise = false;
+            penCell.Dispose();
+            ReDrawPicture();
         }
     }
 }
