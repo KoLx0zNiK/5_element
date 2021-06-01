890226c2e0842fcea5d54da6b1a52f3621fa09e9
Neo <Neo@DESKTOP-H6916IN>
2021-06-01 17:28:24 +0300

    Кто прочетал тот сдохнет((

--- UserControl1.Designer.cs
+++ UserControl1.Designer.cs
@@ -31,6 +31,12 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
@@ -40,11 +46,16 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.White;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Location = new System.Drawing.Point(219, 109);
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pictureBox1.Location = new System.Drawing.Point(214, 114);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(325, 154);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseWheel);
             // 
             // trackBar1
             // 
@@ -55,6 +66,7 @@
             this.trackBar1.Size = new System.Drawing.Size(732, 56);
             this.trackBar1.TabIndex = 1;
             this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
             // trackBar2
             // 
@@ -66,18 +78,81 @@
             this.trackBar2.Size = new System.Drawing.Size(56, 382);
             this.trackBar2.TabIndex = 2;
             this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(511, 335);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Установить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(511, 365);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Удалить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(639, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(639, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(977, 144);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Вернуть";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(899, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
             // 
             // MyControlElement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Aqua;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.trackBar2);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.pictureBox1);
             this.MinimumSize = new System.Drawing.Size(55, 55);
             this.Name = "MyControlElement";
-            this.Size = new System.Drawing.Size(800, 450);
+            this.Size = new System.Drawing.Size(1216, 670);
             this.Load += new System.EventHandler(this.MyControlElement_Load);
             this.Resize += new System.EventHandler(this.MyControlElement_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -93,5 +168,11 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
     }
 }
--- UserControl1.cs
+++ UserControl1.cs
@@ -6,11 +6,46 @@ namespace _5_element
 {
     public partial class MyControlElement : UserControl
     {
+
+
+        //Xscr=Kx(x-min);Yscr=bmp.Height-Ky(y-Ymin)
+
+        //zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+
         Bitmap bmp;
         Graphics gr;
         int cellX, cellY;
-        bool choise = false;
+        bool choiseC = false;
         Pen penCell;
+
+        //zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+
+        bool choiseG = false;
+        double x, y, xxx;
+        int x1scr, y1scr, x2scr, y2scr;
+        double yscr;
+        double kx, ky;
+        double xbasmin = -1, xbasmax = 1;
+        double ybasmin = -1, ybasmax = 1;
+        double xmin, xmax;
+        double ymin, ymax;
+        int degree;
+        double[] koef;
+        Pen penGraph;
+        public double Ahor = 1.1, Av = 1.1, Ak = 1.1;
+        double k = 1;
+
+        //zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+
+        bool choiseP = false;
+        int num = 0;
+        double[] Xpoint, Ypoint;
+        SolidBrush kistochka;
+        public int typepoints = 0;
+        public int size = 5;
+
+
+
         public MyControlElement()
         {
             InitializeComponent();
@@ -29,11 +64,38 @@ namespace _5_element
             trackBar1.Top = pictureBox1.Height + 5;
             trackBar1.Width = pictureBox1.Width;
 
-            //zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
-
             trackBar2.Left = pictureBox1.Width + 5;
             trackBar2.Top = 0;
             trackBar2.Height = pictureBox1.Height;
+
+            //zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+
+            button1.Left = 0;
+            button1.Top = 0;
+            button1.Width = (trackBar1.Width / 6);
+
+            button2.Left = 0;
+            button2.Top = button1.Height;
+            button2.Width = (trackBar1.Width / 6);
+
+            button3.Left =0;
+            button3.Top = button1.Height + button2.Height;
+            button3.Width = (trackBar1.Width/6);
+
+            //zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+
+            textBox1.Left = button1.Width + 5;
+            textBox1.Top = 0;
+            textBox1.Width = button1.Width;
+
+            textBox2.Left = button1.Width + 5;
+            textBox2.Top = button1.Height;
+            textBox2.Width = button1.Width;
+
+            //zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+
+            button2.Enabled = false;
+            button3.Enabled = false;
         }
 
         private void MyControlElement_Resize(object sender, EventArgs e)
@@ -49,17 +111,41 @@ namespace _5_element
             trackBar1.Top = pictureBox1.Height + 5;
             trackBar1.Width = pictureBox1.Width;
 
-            //zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
-
             trackBar2.Left = pictureBox1.Width + 5;
             trackBar2.Top = 0;
             trackBar2.Height = pictureBox1.Height;
+
+            //zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+
+            button1.Left = 0;
+            button1.Top = 0;
+            button1.Width = (trackBar1.Width / 6);
+
+            button2.Left = 0;
+            button2.Top = button1.Height;
+            button2.Width = (trackBar1.Width / 6);
+
+            button3.Left = 0;
+            button3.Top = button1.Height + button2.Height;
+            button3.Width = (trackBar1.Width / 6);
+
+            //zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+
+            textBox1.Left = button1.Width + 5;
+            textBox1.Top = 0;
+            textBox1.Width = button1.Width;
+
+            textBox2.Left = button1.Width + 5;
+            textBox2.Top = button1.Height;
+            textBox2.Width = button1.Width;
         }
 
         private void ReDrawPicture()
         {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            gr = Graphics.FromImage(bmp);
             gr.Clear(pictureBox1.BackColor);
-            if (choise)
+            if (choiseC)
             {
                 for (int i = 0; i <= cellX; i++)
                 {
@@ -70,34 +156,254 @@ namespace _5_element
                     gr.DrawLine(penCell, 0, (int)(bmp.Height * i / cellY), bmp.Width, (int)(bmp.Height * i / cellY));
                 }
             }
+            if (choiseG)
+            {
+                xmin = ((xbasmin + xbasmax) / 2) - ((xbasmax - xbasmin) / 2) * k * Math.Pow(Ahor, trackBar1.Value);
+                xmax = ((xbasmin + xbasmax) / 2) + ((xbasmax - xbasmin) / 2) * k * Math.Pow(Ahor, trackBar1.Value);
+                ymin = ((ybasmin + ybasmax) / 2) - ((ybasmax - ybasmin) / 2) * k * Math.Pow(Av, trackBar2.Value);
+                ymax = ((ybasmin + ybasmax) / 2) + ((ybasmax - ybasmin) / 2) * k * Math.Pow(Av, trackBar2.Value);
+                kx = bmp.Width / (xmax - xmin);
+                ky = bmp.Height / (ymax - ymin);
 
+                x = xmin;
+                y = 0;
+                xxx = 1;
+                for (int i = degree; i >= 0; i--)
+                {
+                    y += koef[i] * xxx;
+                    xxx *= x;
+                }
+                x1scr = 0;
+                yscr = (bmp.Height - ky * (y - ymin));
+                if (yscr < 0)
+                {
+                    y1scr = -1;
+                }
+                else
+                {
+                    if (yscr > bmp.Height)
+                    {
+                        y1scr = bmp.Height + 1;
+                    }
+                    else
+                    {
+                        y1scr = (int)yscr;
+                    }
+                }
+                for (int i = 1; i <= bmp.Width; i++)
+                {
+                    x2scr = i;
+                    x = xmin + x2scr / kx;
+                    y = 0;
+                    xxx = 1;
+                    for(int j = degree; j>= 0; j--) 
+                    {
+                        y += koef[j] * xxx;
+                        xxx *= x;
+                    }
+                    //y2scr = (int)(bmp.Height - ky * (y - ymin));
+                    yscr = (bmp.Height - ky * (y - ymin));
+                    if (yscr < 0)
+                    {
+                        y2scr = -1;
+                    }
+                    else
+                    {
+                        if (yscr > bmp.Height)
+                        {
+                            y2scr = bmp.Height + 1;
+                        }
+                        else
+                        {
+                            y2scr = (int)yscr;
+                        }
+                    }
+                    gr.DrawLine(penGraph, x1scr, y1scr, x2scr, y2scr);
+                    x1scr = x2scr;
+                    y1scr = y2scr;
+                }
+                if (choiseP)
+                {
+                    for (int i = 0; i < num; i++)
+                    {
+                        int Xscr = (int)(kx * (Xpoint[i] - xmin));
+                        int Yscr = (int)(bmp.Height - ky * (Ypoint[i] - ymin));
+                        switch (typepoints)
+                        {
+                            case 0:
+                                gr.FillEllipse(kistochka, Xscr - size, Yscr - size, 2 * size, 2 * size);
+                                break;
+                            case 1:
+                                gr.FillRectangle(kistochka, Xscr - size, Yscr - size, 2 * size, 2 * size);
+                                break;
 
+                        }
+                    }
 
-
-
+                }
+                button3.Enabled = true;
+            }
             pictureBox1.Image = bmp;
         }
-        public void InitGraph()
+
+        //
+
+        public void addPoints(int N, double[] X, double[] Y, Color cl)
         {
-            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-            gr = Graphics.FromImage(bmp);
+            choiseP = true;
+            num = N;
+            Xpoint = new double[N];
+            Ypoint = new double[N];
+            kistochka = new SolidBrush(cl);
+            for (int i = 0; i < N; i++)
+            {
+                Xpoint[i] = X[i];
+                Ypoint[i] = Y[i];
+            }
+            ReDrawPicture();
+        }
+        public void deletePoints()
+        {
+            choiseP = false;
+            kistochka.Dispose();
+            Array.Clear(Xpoint,0,num);
+            Array.Clear(Ypoint, 0, num);
+            ReDrawPicture();
+        }
+        public void addGraph(int n, double[] A, Color cl, int ww)
+        {
+            koef = new double[n + 1];
+            for (int i = 0; i <= n;i++)
+            {
+                koef[i] = A[i];
+            }
+            penGraph = new Pen(cl, ww);
+            degree = n;
+            choiseG = true;
+            ReDrawPicture();
+        }
+        public void deleteGraph()
+        {
+            penGraph.Dispose();
+            choiseG = false;
+            ReDrawPicture();
         }
         public void SetBackColor(Color cl)
         {
             pictureBox1.BackColor = cl;
         }
-        public void Addcell(int x, int y, Color cl, int W)
+
+        private void pictureBox1_MouseWheel(object sender, MouseEventArgs e)
         {
-            choise = true;
-            penCell = new Pen(cl, W);
-            cellX = x;
-            cellY = y;
+            if (choiseG)
+            {
+                label1.BackColor = pictureBox1.BackColor;
+                label1.Left = e.X;
+                label1.Top = e.Y + 10;
+                label1.Text = string.Format("( {0:f2}, {1:f2})", xmin + (e.X / kx), ymin + ((bmp.Height - e.Y) / ky));
+                k *= Math.Pow(Ak, e.Delta / 100);
+                ReDrawPicture();
+            }
+        }
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            label1.Visible = choiseG;//если есть график, то и  метка есть  
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (choiseG)
+            {
+                label1.BackColor = pictureBox1.BackColor;
+                label1.Left = e.X;
+                label1.Top = e.Y + 10;
+                label1.Text = string.Format("( {0:f2}, {1:f2})", xmin + (e.X / kx), ymin + ((bmp.Height - e.Y) / ky));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string x;
+            x = textBox1.Text;
+            bool bla = int.TryParse(x, out cellX);
+            string y;
+            y = textBox2.Text;
+            bool bla2 = int.TryParse(y, out cellY);
+            if (bla & bla2)
+            { 
+                cellX = Convert.ToInt32(textBox1.Text);
+                cellY = Convert.ToInt32(textBox2.Text);
+
+                if (cellX < 0 | cellY < 0)
+                {
+                    DialogResult dr = MessageBox.Show("Введено отрицательное число. Использовать модуль?", "Ошибка", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+                    if (dr == DialogResult.Yes)
+                    {
+                        cellX = Math.Abs(Convert.ToInt32(textBox1.Text));
+                        cellY = Math.Abs(Convert.ToInt32(textBox2.Text));
+                        choiseC = true;
+                        penCell = new Pen(Color.Black, 1);
+                        ReDrawPicture();
+                        button2.Enabled = true;
+                    }
+                    else
+                    {
+                        textBox1.Text = (" ");
+                        textBox2.Text = (" ");
+                        choiseC = false;
+                        penCell.Dispose();
+                        ReDrawPicture();
+                    }
+                }
+                else
+                {
+                    choiseC = true;
+                    penCell = new Pen(Color.Black, 1);
+                    ReDrawPicture();
+                    button2.Enabled = true;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите число", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox1.Text = (" ");
+                textBox2.Text = (" ");
+                choiseC = false;
+                ReDrawPicture();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            choiseC = false;
+            penCell.Dispose();
+            ReDrawPicture();
+            textBox1.Text = (" ");
+            textBox2.Text = (" ");
+            button2.Enabled = false;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            trackBar1.Value = 0;
+            trackBar2.Value = 0;
+            k = 1;
             ReDrawPicture();
         }
-        public void DeleteCell()
+
+
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
         {
-            choise = false;
-            penCell.Dispose();
+            ReDrawPicture();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
             ReDrawPicture();
         }
     }
